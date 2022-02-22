47d14e9d3759a305421ddc2e7efa8803c8bcc383
vu9huy <gaaravu@gmail.com>
2022-02-22 17:02:46 +0700

    second-commit

--- src/Components/Body/OwlWrapper/OwlWrapper.js
+++ src/Components/Body/OwlWrapper/OwlWrapper.js
@@ -1,4 +1,5 @@
 import './OwlWrapper.scss';
+import './react-awesome-lightbox-style.scss';
 import owlImage1 from '../../../assets/image/owl-image-01.jpg';
 import owlImage2 from '../../../assets/image/owl-image-02.jpg';
 import owlImage3 from '../../../assets/image/owl-image-03.jpg';
@@ -6,14 +7,12 @@ import owlImage4 from '../../../assets/image/owl-image-04.jpg';
 import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-import { useState } from 'react';
-
-
-// import Lightbox from "react-awesome-lightbox";
+import { useState, useRef } from 'react';
+import Lightbox from "react-awesome-lightbox";
 // import "react-awesome-lightbox/build/style.css";
 
 const OwlWrapper = () => {
-    const owlImagesList = [
+    let owlImagesList = [
         owlImage1,
         owlImage2,
         owlImage3,
@@ -27,26 +26,66 @@ const OwlWrapper = () => {
         slidesToScroll: 1,
         dotClass: 'dots'
     };
-    // const [isImageViewerOpen, setIsImageViewerOpen] = useState(false)
+    const [isImageViewerOpen, setIsImageViewerOpen] = useState(false)
+    const test = useRef(null);
+    const [imageTest, setImageTest] = useState(owlImagesList);
 
+    function handleClick(e) {
+        setIsImageViewerOpen(true);
+        // console.log(e.target.src);
+        // console.log(e.target.baseURI.slice(0, -1) + owlImage2);
+        if (e.target.src == (e.target.baseURI.slice(0, -1) + owlImage1)) {
+            setImageTest([
+                owlImage1,
+                owlImage2,
+                owlImage3,
+                owlImage4
+            ])
+        }
+        if (e.target.src == (e.target.baseURI.slice(0, -1) + owlImage2)) {
+            setImageTest([
+                owlImage2,
+                owlImage3,
+                owlImage4,
+                owlImage1
+            ])
+        }
+        if (e.target.src == (e.target.baseURI.slice(0, -1) + owlImage3)) {
+            setImageTest([
+                owlImage3,
+                owlImage4,
+                owlImage1,
+                owlImage2
+            ])
+        }
+        if (e.target.src == (e.target.baseURI.slice(0, -1) + owlImage4)) {
+            setImageTest([
+                owlImage4,
+                owlImage1,
+                owlImage2,
+                owlImage3
+            ])
+        }
+
+    }
     return (
         <div className='owl-wrapper' >
-            {/* {isImageViewerOpen && (
-                <Lightbox
-                    images={images}
-                    startIndex={imageIndex}
-                // onClose={() => setIsImageViewerOpen(false)}
-                />
-            )} */}
             <Slider {...settings}>
                 {owlImagesList.map((owlImage, index) => {
                     return (
                         <div className='owl-item' key={index}>
-                            <img src={owlImage} />
+                            <img src={owlImage} onClick={(e) => handleClick(e)} />
                         </div>
                     )
                 })}
             </Slider>
+            {isImageViewerOpen && (
+                <Lightbox
+                    images={imageTest}
+                    onClose={() => setIsImageViewerOpen(false)}
+                />
+            )}
+
         </div>
     )
 }
